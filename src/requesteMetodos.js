713f956b19915d5c2b9a05c6ff0249a242857f3e
jeftejb <jeftesambangojb@gmail.com>
2022-04-28 09:35:13 +0100

    my first commit

--- src/requesteMetodos.js
+++ src/requesteMetodos.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const TOKEN = ()=>{ return localStorage.getItem("persist:root")?.user?.currentUser?.acessoToken }
+
 /*
 if(localStorage.getItem("persist:root")!== null){
     var TOKEN =  current?.acessoToken
@@ -7,12 +7,12 @@ if(localStorage.getItem("persist:root")!== null){
        TOKEN =" "   
    };
    */
-
+const TOKEN =  localStorage.getItem("persist:root")?.user?.currentUser?.acessoToken ;
 const BASE_URL = "https://vandjaline-f.herokuapp.com/api/" // "http://localhost:8080/api/" ;
 export const publicRequest = axios.create({
     baseURL:BASE_URL,
 });
 export const userRequest = axios.create({
     baseURL:BASE_URL,
-    headers:{token:`Bearer ${TOKEN()}`}
+    headers:{token:`Bearer ${TOKEN}`}
 });
