2412befba9013ee54cdd9cfd3c34296c08f414d6
jeftejb <46708904+jeftejb@users.noreply.github.com>
2023-04-28 08:03:37 +0100

    Update requesteMetodos.js

--- src/requesteMetodos.js
+++ src/requesteMetodos.js
@@ -12,8 +12,8 @@ if(localStorage.getItem("persist:root")!== null){
 if (localStorage.getItem("persist:root")){
 var TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)?.currentUser?.acessoToken;
 }
-//const URL = process.env.REACT_APP_API_URL  
-const URL = process.env.REACT_APP_BASE_URL
+const URL = process.env.REACT_APP_API_URL  
+//const URL = process.env.REACT_APP_BASE_URL
 
 export const publicRequest = axios.create({
     baseURL:URL,
